8d07bb60da0e02a4273f7dba040070c39f06c6ff
unknown <dorman.urfi@gmail.com>
2023-02-14 12:15:59 +0100

    ne znam

---
+++ script.js
@@ -0,0 +1,24 @@
+console.dir(document);
+//document.title="cigoo"
+//docuemnt.log(document.head)
+//console.log(document.getElementById("main_title"))
+//document.getElementById("main_title").textContent="3 naslov"
+//let cim=document.getElementById("main_title")
+//cim.textContent="4 naslov"
+/*for(let i =0;i<items.lenght;i++){
+    if(i%2==0){
+        items[i].style.backgroundColor="lightgrey";
+    }
+}
+*/
+//console.log(document.getElementsByTagName("h2"))
+//let h2=document.getElementsByTagName
+//id #
+//class .
+console.log(document.querySelector("elso a"))
+let elsolink=document.querySelector("#elso a");
+elsolink.style.color="red";
+
+console.log(document.querySelector("elso p"))
+let elsolink=document.querySelector("#elso p");
+elsolink.style.color="red";
